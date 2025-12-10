269088915d013c1be3186e65cfdbdec1e3fe8180
Stanisław Sklepowicz <stanislaw.sklepowicz@gmail.com>
2025-12-10 17:33:25 +0100

    Added constructors to Display class

---
+++ Display.cpp
@@ -0,0 +1,13 @@
+#include "Display.h"
+
+Display::Display()
+{
+	windowWidth = 1280;
+	windowHeight = 720;
+}
+
+Display::Display(int width, int height)
+{
+	windowWidth = width;
+	windowHeight = height;
+}
--- Display.h
+++ Display.h
@@ -2,6 +2,10 @@
 #define H_DISPLAY
 class Display
 {
-
+	int windowWidth;
+	int windowHeight;
+public:
+	Display();
+	Display(int width, int height);
 };
 #endif
