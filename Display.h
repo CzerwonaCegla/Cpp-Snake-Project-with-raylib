068eebc5d9e20c827cb8343f0f10c0b8b79d2884
Stanisław Sklepowicz <stanislaw.sklepowicz@gmail.com>
2025-12-10 17:54:51 +0100

    Started methods for display management

--- Display.h
+++ Display.h
@@ -1,11 +1,16 @@
 #ifndef H_DISPLAY
 #define H_DISPLAY
+#include <string>
 class Display
 {
 	int windowWidth;
 	int windowHeight;
+	std::string windowName;
 public:
 	Display();
-	Display(int width, int height);
+	Display(int width, int height, std::string name);
+	void startDisplay();
+	void renderDisplay();
+	void endDisplay();
 };
 #endif
