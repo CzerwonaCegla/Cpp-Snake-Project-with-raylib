48bb715559491ee9413b4d42a86e49bbac016bf6
Stanisław Sklepowicz <stanislaw.sklepowicz@gmail.com>
2025-12-10 18:09:06 +0100

    Started prototyping everything in Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,38 @@
+#include <string>
+#include <vector>
 #include <raylib.h>
+
+class Snake
+{
+	int length;
+	std::vector<Vector2> snakeCoords;
+	Vector2 currentDirection;
+public:
+	Snake()
+	{
+		length = 1;
+		Vector2 initialSnakePos;
+		initialSnakePos.x = 0;
+		initialSnakePos.y = 0;
+		snakeCoords.push_back(initialSnakePos);
+		currentDirection.x = 0;
+		currentDirection.y = 0;
+	}
+};
 
 int main()
 {
+	int height = 720;
+	int width = 1280;
+	std::string name = "Snake";
+
+	InitWindow(width, height, name.c_str());
+
+	while (!WindowShouldClose())
+	{
+
+	}
+
+	CloseWindow();
 	return 0;
 }
