edb493f5dae2afbd2de7790a0bd1cbe68defe030
Stanisław Sklepowicz <stanislaw.sklepowicz@gmail.com>
2025-12-11 01:21:17 +0100

    Further improvements to early key input

--- Main.cpp
+++ Main.cpp
@@ -28,8 +28,8 @@ public:
 
 int main()
 {
-	int height = 720;
-	int width = 1280;
+	int height = 700;
+	int width = 1200;
 	std::string name = "Snake";
 	SetTargetFPS(60);
 	InitWindow(width, height, name.c_str());
@@ -39,10 +39,10 @@ int main()
 	Vector2 addToPos = { 0.0, 0.0 };
 	float t = 0.0;
 	float moveToPosTime = 0.4;
+	int loggedKey = 0;
 	while (!WindowShouldClose())
 	{
 		// log keys earlier than meets conditions for better input
-		int loggedKey = 0;
 		if(IsKeyDown(KEY_UP)){loggedKey = 1;}
 		else if(IsKeyDown(KEY_DOWN)) { loggedKey = 2; }
 		else if (IsKeyDown(KEY_RIGHT)) { loggedKey = 3; }
@@ -55,10 +55,11 @@ int main()
 			t = 0.0;
 
 			// check for inputs
-			if (/*IsKeyDown(KEY_UP)*/ loggedKey == 1) { addToPos = {0.0, 0.0}; addToPos.y = -50;}
-			else if (/*IsKeyDown(KEY_DOWN)*/ loggedKey == 2) { addToPos = { 0.0, 0.0 }; addToPos.y = 50.0;}
-			else if (/*IsKeyDown(KEY_RIGHT)*/ loggedKey == 3) { addToPos = { 0.0, 0.0 }; addToPos.x = 50.0;}
-			else if (/*IsKeyDown(KEY_LEFT)*/ loggedKey == 4) { addToPos = { 0.0, 0.0 }; addToPos.x = -50.0;}
+			if (/*IsKeyDown(KEY_UP) || */loggedKey == 1) { addToPos = {0.0, 0.0}; addToPos.y = -50;}
+			else if (/*IsKeyDown(KEY_DOWN) || */loggedKey == 2) { addToPos = { 0.0, 0.0 }; addToPos.y = 50.0;}
+			else if (/*IsKeyDown(KEY_RIGHT) || */loggedKey == 3) { addToPos = { 0.0, 0.0 }; addToPos.x = 50.0;}
+			else if (/*IsKeyDown(KEY_LEFT) || */loggedKey == 4) { addToPos = { 0.0, 0.0 }; addToPos.x = -50.0;}
+			loggedKey = 0;
 			targetPos += addToPos;
 		}
 		if (t < 1.0) { t += GetFrameTime()/moveToPosTime; }
