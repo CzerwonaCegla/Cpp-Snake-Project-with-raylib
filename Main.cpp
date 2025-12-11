be26bb3d6b9c402a294cecc59acdb5db1480587a
Stanisław Sklepowicz <stanislaw.sklepowicz@gmail.com>
2025-12-11 01:17:17 +0100

    Added better snake movement by lerp and earlier key input

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <raylib.h>
+#include <raymath.h>
 
 class Snake
 {
@@ -30,23 +31,44 @@ int main()
 	int height = 720;
 	int width = 1280;
 	std::string name = "Snake";
-	SetTargetFPS(5);
+	SetTargetFPS(60);
 	InitWindow(width, height, name.c_str());
-	Vector2 currentDirection = { 0.0,0.0 };
-	Vector2 objPos = { 200.0, 200.0 };
+	Vector2 objPos = { 0.0, 0.0 };
+	Vector2 currentPos = objPos;
+	Vector2 targetPos = { 0.0,0.0 };
+	Vector2 addToPos = { 0.0, 0.0 };
+	float t = 0.0;
+	float moveToPosTime = 0.4;
 	while (!WindowShouldClose())
 	{
-		//currentDirection = { 0.0,0.0 };
-		if (IsKeyDown(KEY_UP)) { currentDirection.y = -50.0; currentDirection.x = 0.0; }
-		else if (IsKeyDown(KEY_DOWN)) { currentDirection.y = 50.0; currentDirection.x = 0.0; }
-		else if (IsKeyDown(KEY_RIGHT)) { currentDirection.x = 50.0; currentDirection.y = 0.0; }
-		else if (IsKeyDown(KEY_LEFT)) { currentDirection.x = -50.0; currentDirection.y = 0.0; }
+		// log keys earlier than meets conditions for better input
+		int loggedKey = 0;
+		if(IsKeyDown(KEY_UP)){loggedKey = 1;}
+		else if(IsKeyDown(KEY_DOWN)) { loggedKey = 2; }
+		else if (IsKeyDown(KEY_RIGHT)) { loggedKey = 3; }
+		else if (IsKeyDown(KEY_LEFT)) { loggedKey = 4; }
+
+		if (Vector2Distance(currentPos, targetPos) < 0.01)
+		{
+			// snap objPos to targetPos when close
+			objPos = targetPos;
+			t = 0.0;
+
+			// check for inputs
+			if (/*IsKeyDown(KEY_UP)*/ loggedKey == 1) { addToPos = {0.0, 0.0}; addToPos.y = -50;}
+			else if (/*IsKeyDown(KEY_DOWN)*/ loggedKey == 2) { addToPos = { 0.0, 0.0 }; addToPos.y = 50.0;}
+			else if (/*IsKeyDown(KEY_RIGHT)*/ loggedKey == 3) { addToPos = { 0.0, 0.0 }; addToPos.x = 50.0;}
+			else if (/*IsKeyDown(KEY_LEFT)*/ loggedKey == 4) { addToPos = { 0.0, 0.0 }; addToPos.x = -50.0;}
+			targetPos += addToPos;
+		}
+		if (t < 1.0) { t += GetFrameTime()/moveToPosTime; }
+		currentPos = Vector2Lerp(objPos, targetPos, t);
+		// Draw start -------------------
 		BeginDrawing();
 		ClearBackground(DARKGREEN);
-		objPos.x += currentDirection.x;
-		objPos.y += currentDirection.y;
-		DrawRectangle(objPos.x, objPos.y, 50, 50, BLUE);
+		DrawRectangle(currentPos.x, currentPos.y, 50, 50, BLUE);
 		EndDrawing();
+		// Draw end ---------------------
 	}
 
 	CloseWindow();
