70c2e2d2bd83b4adebab0c3b71b9c924ed5a240b
Stanisław Sklepowicz <stanislaw.sklepowicz@gmail.com>
2025-12-17 18:14:19 +0100

    Removed redundant code and changed snake colour

--- Main.cpp
+++ Main.cpp
@@ -17,6 +17,7 @@ class Snake
 	Vector2 previouslyAddedToTargetVector;
 	Vector2 tailSpawnPos;
 
+	// Reads input and 
 	Vector2 getTargetVectorFromInput()
 	{
 		Vector2 localTargetVector = { 0,0 };
@@ -94,95 +95,51 @@ public:
 			exit(0);
 		}
 		
-		/*for (auto iter = snakePartsCoords.rbegin(); iter != snakePartsCoords.rend(); ++iter)*/
 		for(int i = (snakePartsCoords.size() - 1); i >= 0; --i)
 		{
 			// If iter is snake's head then set it to target vector
 			// Else set part to vector of the part closer to the snake's head
-			if (/*iter == (snakePartsCoords.rend() - 1)*/i==0)
+			if (i == snakePartsCoords.size() - 1) { tailSpawnPos = snakePartsCoords[i]; }
+			if (i==0)
 			{
 				snakePartsCoords[0] = targetVector;
-				/**iter = targetVector;*/
 			}
 			else
 			{
 				snakePartsCoords[i] = snakePartsCoords[i - 1];
-				/**iter = *(iter + 1);*/
 			}
 		}
 	}
 
+	// Loops through all elements in vector and draws them
 	void drawSnake()
 	{
 		for (const auto part : snakePartsCoords)
 		{
-			DrawRectangle(part.x, part.y, GRID_WIDTH, GRID_WIDTH, SKYBLUE);
+			DrawRectangle(part.x, part.y, GRID_WIDTH, GRID_WIDTH, BLUE);
 		}
 	}
 };
 
 int main()
 {
-	//int height = 750;
-	//int width = 1250;
 	std::string name = "Snake";
 	const int gridWidth = GRID_WIDTH;
 	SetTargetFPS(1);
 	InitWindow(SNAKE_WINDOW_WIDTH, SNAKE_WINDOW_HEIGHT, name.c_str());
 	// DO NOT TOUCH CODE ABOVE FOR NOW
 
-	/*Vector2 objPos = { 0.0, 0.0 };
-	Vector2 currentPos = objPos;
-	Vector2 targetPos = { 0.0,0.0 };
-	Vector2 addToPos = { 0.0, 0.0 };
-	float t = 0.0;
-	float moveToPosTime = 0.5;
-	int loggedKey = 0;*/
 
-	// MAIN GAME LOOP DO NOT TOUCH
 	Snake snake;
+	// MAIN GAME LOOP
 	while (!WindowShouldClose())
 	{
-		
-		// log keys earlier than meets conditions for better input
-		//if(IsKeyDown(KEY_UP)){loggedKey = 1;}
-		//else if(IsKeyDown(KEY_DOWN)) { loggedKey = 2; }
-		//else if (IsKeyDown(KEY_RIGHT)) { loggedKey = 3; }
-		//else if (IsKeyDown(KEY_LEFT)) { loggedKey = 4; }
-
-		//if (Vector2Distance(currentPos, targetPos) < 0.05)
-		//{
-		//	// snap objPos to targetPos when close
-		//	/*objPos = targetPos;
-		//	t = 0.0;*/
-
-		//	// check for inputs
-		//	//if (/*IsKeyDown(KEY_UP) || */loggedKey == 1) { addToPos = { 0.0, 0.0 }; addToPos.y = -gridWidth; }
-		//	//else if (/*IsKeyDown(KEY_DOWN) || */loggedKey == 2) { addToPos = { 0.0, 0.0 }; addToPos.y = gridWidth;}
-		//	//else if (/*IsKeyDown(KEY_RIGHT) || */loggedKey == 3) { addToPos = { 0.0, 0.0 }; addToPos.x = gridWidth;}
-		//	//else if (/*IsKeyDown(KEY_LEFT) || */loggedKey == 4) { addToPos = { 0.0, 0.0 }; addToPos.x = -gridWidth;}
-		//	//loggedKey = 0;
-		//	//targetPos += addToPos;
-		//	//if (targetPos.x < 0.0 || targetPos.x > (float)(width-gridWidth)) { targetPos.x -= addToPos.x; }
-		//	//if (targetPos.y < 0.0 || targetPos.y > (float)(height-gridWidth)) { targetPos.y -= addToPos.y; }
-		//}
-		//else
-		//{
-		//	if (t < 1.0) { t += GetFrameTime()/moveToPosTime; }
-		//	if (t > 1.0) { t = 1.0; }
-		//	currentPos = Vector2Lerp(objPos, targetPos, t);
-		//}
-
-
 		snake.moveSnake();
 
-		// DO NOT TOUCH CODE BELOW FOR NOW
 		// Draw start -------------------
 		BeginDrawing();
+
 		ClearBackground(DARKGREEN);
-
-
-		//DrawRectangle(currentPos.x, currentPos.y, gridWidth, gridWidth, BLUE);
 		snake.drawSnake();
 
 		EndDrawing();
