c42bbdf11cedca65ed603d545d071c4455909bd5
Stanisław Sklepowicz <stanislaw.sklepowicz@gmail.com>
2026-01-03 16:45:45 +0100

    Added random rolling for apple coordinates

--- Apple.cpp
+++ Apple.cpp
@@ -1,18 +1,42 @@
 #include <raylib.h>
 #include "Apple.h";
+#include "GlobalGameParameters.h"
 
 Apple::Apple()
 {
 	appleTexture = LoadTexture("Textures\\Apple.png");
-	coordinates = { 500, 500 };
+	coordinates = rollCoordinates();
 }
 
 Apple::Apple(Vector2 coordinates)
 {
-
+	appleTexture = LoadTexture("Textures\\Apple.png");
+	this->coordinates = coordinates;
 }
 
 void Apple::drawObject()
 {
 	DrawTextureV(appleTexture, coordinates, WHITE);
+}
+
+Vector2 Apple::getCoordinates()
+{
+	return coordinates;
+}
+
+Vector2 Apple::rollCoordinates()
+{
+	int xMult = (globalGameWindowWidth-globalGridWidth) / globalGridWidth;
+	int x = GetRandomValue(0, xMult);
+
+	int yMult = (globalGameWindowHeight-globalGridWidth) / globalGridWidth;
+	int y = GetRandomValue(0, yMult);
+
+	Vector2 tempVec2 = { (float)x * globalGridWidth, (float)y * globalGridWidth };
+	return tempVec2;
+}
+
+void Apple::setNewCoordinates()
+{
+	coordinates = rollCoordinates();
 }
--- Apple.h
+++ Apple.h
@@ -7,11 +7,14 @@ class Apple : RenderableObject
 {
 	Texture2D appleTexture;
 	Vector2 coordinates;
+	Vector2 rollCoordinates();
 
 public:
 	Apple();
 	Apple(Vector2 coords);
 	void drawObject() override;
+	Vector2 getCoordinates();
+	void setNewCoordinates();
 };
 
 #endif // !H_APPLE
