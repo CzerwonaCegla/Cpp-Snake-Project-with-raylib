5ce710f0ebd29c9c2c6effc43f3aab8360622430
Stanisław Sklepowicz <stanislaw.sklepowicz@gmail.com>
2026-01-03 16:49:11 +0100

    Removed unused code from game parameters

--- GlobalGameParameters.h
+++ GlobalGameParameters.h
@@ -1,9 +1,6 @@
 #ifndef H_GLOBAL_GAME_PARAMETERS
 #define H_GLOBAL_GAME_PARAMETERS
 
-//extern const int globalGridWidth;
-//extern const int globalGameWindowWidth;
-//extern const int globalGameWindowHeight;
 constexpr int globalGridWidth = 50;
 constexpr int globalGameWindowWidth = 1300;
 constexpr int globalGameWindowHeight = 800;
