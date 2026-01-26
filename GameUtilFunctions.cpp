980141d0aa8c67116fc1818df09f77cbd2349cac
Stanisław Sklepowicz <stanislaw.sklepowicz@gmail.com>
2026-01-26 16:34:36 +0100

    Code formatting fix

--- GameUtilFunctions.cpp
+++ GameUtilFunctions.cpp
@@ -4,20 +4,20 @@
 
 void drawBackgroundGrid()
 {
-> Vector2 placementVector = { 0,0 };
-> Texture backgroundTexture = LoadTexture("Textures\\GridTile.png");
-> constexpr int xMult = globalGameWindowWidth / 50;
-> constexpr int yMult = globalGameWindowHeight / 50;
-> for (int i = 0; i < yMult; ++i)
-> {
->    for (int j = 0; j < xMult; ++j)
->    {
->        DrawTexture(backgroundTexture, placementVector.x, placementVector.y, WHITE);
->        placementVector.x += globalGridWidth;
->    }
->    placementVector.x = 0;
->    placementVector.y += globalGridWidth;
-> }
+    Vector2 placementVector = { 0,0 };
+    Texture backgroundTexture = LoadTexture("Textures\\GridTile.png");
+    constexpr int xMult = globalGameWindowWidth / globalGridWidth;
+    constexpr int yMult = globalGameWindowHeight / globalGridWidth;
+    for (int i = 0; i < yMult; ++i)
+    {
+        for (int j = 0; j < xMult; ++j)
+        {
+            DrawTexture(backgroundTexture, placementVector.x, placementVector.y, WHITE);
+            placementVector.x += globalGridWidth;
+        }
+        placementVector.x = 0;
+        placementVector.y += globalGridWidth;
+    }
 }
 
 
