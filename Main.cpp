b4de6040ec243cc899ae8214f220190329d79817
Stanisław Sklepowicz <stanislaw.sklepowicz@gmail.com>
2026-01-26 17:13:43 +0100

    Added game state (win, lose)

--- Main.cpp
+++ Main.cpp
@@ -14,6 +14,9 @@ using namespace std;
 
 int main()
 {
+	enum class gameWinState {None, Win, Lose};
+	gameWinState state = gameWinState::None;
+
 	string name = "Snake";
 	constexpr int targetFps = 120;
 	SetTargetFPS(targetFps);
